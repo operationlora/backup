4351f4faedbf12a705a5cfc0f2bf499dbb17162f
unendless314 <65386006+unendless314@users.noreply.github.com>
2025-08-09 22:03:48 -0700

    20250809 testing github pages

--- js/super-search.js
+++ js/super-search.js
@@ -3,7 +3,7 @@ Author: Kushagra Gour (http://kushagragour.in)
 MIT Licensed
 */
 (function () {
-	var searchFile = '/feed.xml',
+	var searchFile = './feed.xml',
 		searchEl,
 		searchInputEl,
 		searchResultsEl,
